9db7fdc283dc036d7c135bace3fe9cfb0db50a6d
Jose Irigon de Irigon <jose.irigon@tu-dresden.de>
2018-08-14 09:37:23 +0200

    adding memory to JVM on one.sh

--- one.sh
+++ one.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xmx512M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -Xmx8000M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar:lib/gson-2.8.5.jar:lib/commons-math3-3.2.jar core.DTNSim $*
